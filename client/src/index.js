d85503ed255a8fb6915373375d5d2f2ea101f409
atiffany <abby.tiffany@gmail.com>
2018-02-20 15:13:49 -0800

    created routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <BrowserRouter>
+    <div>
+      <div>Poor Man's IMDB!</div>
+      <Route exact path = '/' component={MovieList} />
+      <Route path = '/:id' component={Movie} />
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
